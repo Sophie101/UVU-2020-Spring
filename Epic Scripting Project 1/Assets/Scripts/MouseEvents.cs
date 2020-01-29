8fc962eedd1a3463c29b3aa1892c9ba56f5f06f8
Sophie Nicholson <10888877@my.uvu.edu>
2020-01-29 11:19:59 -0700

    Mouse Events

---
+++ Epic Scripting Project 1/Assets/Scripts/MouseEvents.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MouseEvents : MonoBehaviour
+{
+
+    public UnityEvent mouseDownEvent, mouseUpEvent, mouseDragEvent, mouseEnterEvent, mouseExitEvent, mouseOverEvent;
+
+ private void OnMouseDown()
+  {
+      mouseDownEvent.Invoke();
+  } 
+  private void OnMouseUp()
+  {
+      mouseUpEvent.Invoke();
+  }
+  private void OnMouseDrag()
+  {
+      mouseDragEvent.Invoke();
+  }
+  private void OnMouseEnter()
+  {
+      mouseEnterEvent.Invoke();
+  }
+  private void OnMouseExit()
+  {
+      mouseExitEvent.Invoke();
+  }
+  private void OnMouseOver()
+  {
+      mouseOverEvent.Invoke();
+  }
+}
