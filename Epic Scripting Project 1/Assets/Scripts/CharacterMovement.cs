e88f2ecbe43b23d7a211f7656b5f36f09677380c
Sophie Nicholson <10888877@my.uvu.edu>
2020-02-03 11:24:52 -0700

    Added a character Controller

---
+++ Epic Scripting Project 1/Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    private Vector3 positionDirection;
+    public float speed = 5f;
+    public float gravity = -3f;
+    public float jumpForce = 10f;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        positionDirection.x = Input.GetAxis("Vertical")*speed;
+        if(Input.GetButtonDown("Jump"))
+        {
+            positionDirection.y = jumpForce;
+        }
+        positionDirection.y += gravity;
+        controller.Move(positionDirection*Time.deltaTime);
+    }
+}
