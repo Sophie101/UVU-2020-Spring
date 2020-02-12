898b0928c8b777acafda491876cf9976f75d0543
Sophie Nicholson <10888877@my.uvu.edu>
2020-02-12 11:21:26 -0700

    Sprite Controller

---
+++ Epic Scripting Project 1/Assets/Scripts/SpriteController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class SpriteController : MonoBehaviour
+{
+    public Color spriteColor = Color.red;
+    public Color offColor = Color.red;
+    public SpriteRenderer spriteObj;
+
+    void Start()
+    {
+        spriteObj = GetComponent<SpriteRenderer>();
+        spriteObj.color = spriteColor;
+
+    }
+
+    private void OnEnable()
+    {
+        spriteObj.color = spriteColor;
+    }
+
+    private void OnDisable()
+    {
+        spriteObj.color = offColor;
+    }
+
+    void Update()
+    {
+        
+    }
+}
