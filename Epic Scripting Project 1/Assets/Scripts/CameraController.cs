7f15d2ee87b47c00c5c22c4224f259bdc6793d3f
Sophie Nicholson <10888877@my.uvu.edu>
2020-02-12 10:43:19 -0700

    Controller Script warmups

---
+++ Epic Scripting Project 1/Assets/Scripts/CameraController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class CameraController : MonoBehaviour
+{
+    public Camera cameraObj;
+    void Start()
+    {
+        cameraObj = GetComponent<Camera>();
+        cameraObj.depth = 1f;
+    }
+
+    void Update()
+    {
+        
+    }
+}
