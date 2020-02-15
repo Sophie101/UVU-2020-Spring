c46c9c8934f8821da73c2504da1d360031c06596
Sophie Nicholson <10888877@my.uvu.edu>
2020-02-14 19:16:56 -0700

    Followed some tutorials from Unity's YouTube Channel. Made some coffee.

---
+++ Epic Scripting Project 1/Assets/Scripts/BehaviorScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class BehaviorScript : MonoBehaviour
+{
+    public Color onColor = Color.yellow;
+    public Color offColor = Color.magenta;
+    //Script is based on this tutorial: https://www.youtube.com/watch?v=Z0Z7xc18CcA&list=PLX2vGYjWbI0S9-X2Q021GUtolTqbUBB9B
+    //I decided to add a bit of my own scripting at the bottom
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            GetComponent<Renderer>().material.color = Color.red;
+        }
+
+        if(Input.GetKeyDown(KeyCode.G))
+        {
+            GetComponent<Renderer>().material.color = Color.green;
+        }
+
+        if(Input.GetKeyDown(KeyCode.B))
+        {
+            GetComponent<Renderer>().material.color = Color.blue;
+        }
+        
+        if(Input.GetKeyDown(KeyCode.Y))
+        {
+            GetComponent<Renderer>().material.color = onColor;
+        }
+
+        if(Input.GetKeyDown(KeyCode.N))
+        {
+            GetComponent<Renderer>().material.color = offColor;
+        }
+    }
+}
---
+++ Epic Scripting Project 1/Assets/Scripts/HotCoffee.cs
@@ -0,0 +1,35 @@
+﻿//This script is based on this tutorial: https://www.youtube.com/watch?v=PQihrWCOSic&list=PLX2vGYjWbI0S9-X2Q021GUtolTqbUBB9B&index=4
+using UnityEngine;
+
+public class HotCoffee : MonoBehaviour
+{
+    float coffeeTemperature = 85.0f;
+    float hotLimitTemperature = 70.0f;
+    float coldLimitTemperature = 40.0f;
+  
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            TemperatureTest();
+
+        coffeeTemperature -= Time.deltaTime * 5f;
+    }
+
+    void TemperatureTest()
+    {
+        if (coffeeTemperature > hotLimitTemperature)
+        {
+            print("Coffee is too hot.");
+        }
+
+        else if (coffeeTemperature < coldLimitTemperature)
+        {
+            print("Coffee is too cold.");
+        }
+
+        else
+        {
+            print("Coffee is just right.");
+        }
+    }
+}
